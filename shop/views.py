f62153cd7a07d43ad854ec7355130a7415f58a4b
Madoumbia <125297962+Madoumbia@users.noreply.github.com>
2026-03-15 10:56:59 +0100

    Implement article purchase functionality
    Added a new 'acheter' view to handle article purchases.

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Article
 
-# Create your views here.
+def catalogue(request):
+    articles = Article.objects.all()
+    return render(request, 'boutique/catalogue.html', {'articles': articles})
+
+def acheter(request, article_id):
+    if request.method == 'POST':
+        article = get_object_or_404(Article, id=article_id)
+        if article.quantite_disponible > 0:
+            article.quantite_disponible -= 1
+            article.save()
+    return redirect('catalogue')
